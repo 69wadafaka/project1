27f6bea0b9e3642417e5618d29b8dee3c641b66e
Marek Lukasiewicz <marqus34@gmail.com>
2017-01-25 21:07:50 +0100

    poprawilem bledy kompilacji

--- main.cpp
+++ main.cpp
@@ -1,190 +1,191 @@
-#include <iostream>
+﻿#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 char cezar(char kontener, char przesun);
-char odszyfruj(int przesun,char kontener);
+//char odszyfruj(int przesun, char kontener);
 int main()
-{   int p=0;
-    int q=0;
-    int n=1;
-    int przesun=0;
-    char kontener;
-    char klucze[10][10];
-    char oryginaltab[20][10];
-    FILE *oryginal=fopen("oryginal.txt", "r");
-    FILE *klucz=fopen("klucz.txt", "r");
-    FILE *kopia=fopen("kopia.txt", "wt");
-    FILE *szyfr=fopen("szyfr.txt", "wt");
+{
+	int p = 0;
+	int q = 0;
+	int n = 1;
+	int przesun = 0;
+	char kontener;
+	char klucze[10][10];
+	char oryginaltab[20][10];
+	FILE *oryginal = fopen("oryginal.txt", "r");
+	FILE *klucz = fopen("klucz.txt", "r");
+	FILE *kopia = fopen("kopia.txt", "wt");
+	FILE *szyfr = fopen("szyfr.txt", "wt");
 
-    //srand(time(NULL));
-    //n=rand()%11-5;
+	//srand(time(NULL));
+	//n=rand()%11-5;
 
-// TWORZÊ KOPIÊ PLIKU ORYGINAL I SZYFR #CEZAR
-    for(int i=0; i<=127; i++){
-        fscanf(oryginal, "%c", &kontener);
-        printf("%c", kontener);
-
-
-        fprintf(kopia, "%c", kontener);
-        kontener=kontener+n;
+	// TWORZÊ KOPIÊ PLIKU ORYGINAL I SZYFR #CEZAR
+	for (int i = 0; i <= 127; i++) {
+		fscanf(oryginal, "%c", &kontener);
+		printf("%c", kontener);
 
 
-        fprintf(szyfr, "%c", kontener);
-    }
-    printf("\n");
-    printf("\n");
-//ZAPISUJĘ TEKST ORYGINAL DO TABLICY
- while(p<20)
-    {
-        while (q<10)
-        {
-        fscanf(oryginaltab, "%c", &oryginal[p][q]);
-        if(oryginaltab[p][q]!='#')
-       printf("%c", oryginaltab[p][q]);
-
-        if (oryginaltab[p][q] == '#')
-        {
-            p=11;
-            break;
-        }
-        if(oryginaltab[p][q]==32)
-        {
-        oryginaltab[p][q]=0;
-        q=0;
-        break;
-        }
-        else
-        {
-        q++;
-        }
-        }
-        p++;
-    }
+		fprintf(kopia, "%c", kontener);
+		kontener = kontener + n;
 
 
-//ZAPISUJÊ DO TABLICY S£OWA - KLUCZE
-    while(p<10)
-    {
-        while (q<10)
-        {
-        fscanf(klucz, "%c", &klucze[p][q]);
-        if(klucze[p][q]!='#')
-       printf("%c", klucze[p][q]);
+		fprintf(szyfr, "%c", kontener);
+	}
+	printf("\n");
+	printf("\n");
+	//ZAPISUJĘ TEKST ORYGINAL DO TABLICY
+	while (p<20)
+	{
+		while (q<10)
+		{
+			fscanf(oryginal, "%c", &oryginaltab[p][q]);
+			if (oryginaltab[p][q] != '#')
+				printf("%c", oryginaltab[p][q]);
 
-        if (klucze[p][q] == '#')
-        {
-            p=11;
-            break;
-        }
-        if(klucze[p][q]==32)
-        {
-        klucze[p][q]=0;
-        q=0;
-        break;
-        }
-        else
-        {
-        q++;
-        }
-        }
-        p++;
-    }
+			if (oryginaltab[p][q] == '#')
+			{
+				p = 11;
+				break;
+			}
+			if (oryginaltab[p][q] == 32)
+			{
+				oryginaltab[p][q] = 0;
+				q = 0;
+				break;
+			}
+			else
+			{
+				q++;
+			}
+		}
+		p++;
+	}
 
-    p=0;
-    q=0;
-// SPRAWDZAM ZAWARTOŒÆ TABLICY KLUCZE
-printf("\n \n wypisz: \n");
 
- while(p<10)
-    {
-        while (q<10)
-        {
-        if(klucze[p][q]!='#')
-        {
-        printf("%c", klucze[p][q]);
-        }
-        if (klucze[p][q] == '#')
-        {
-            p=11;
-            break;
-        }
-        if(klucze[p][q]==0)
-        {
-        q=0;
-        break;
-        }
-        else
-        {
-        q++;
-        }
-        }
-        p++;
-    }
-       printf("\n---- \n");
-// ZAMYKAM SZYFR I KOPIÊ I OTWIERAM W TRYBIE READ
-    printf("\n");
-    fclose(szyfr);
-    fclose(kopia);
-    kopia=fopen("kopia.txt", "r");
-    szyfr=fopen("szyfr.txt", "r");
+	//ZAPISUJÊ DO TABLICY S£OWA - KLUCZE
+	while (p<10)
+	{
+		while (q<10)
+		{
+			fscanf(klucz, "%c", &klucze[p][q]);
+			if (klucze[p][q] != '#')
+				printf("%c", klucze[p][q]);
 
-    odszyfruj(kontener, przesun);
-    /*
-    for(int i=0; i<=100; i++)
-    {   if()
-        fscanf(szyfr, "%c", &kontener);
+			if (klucze[p][q] == '#')
+			{
+				p = 11;
+				break;
+			}
+			if (klucze[p][q] == 32)
+			{
+				klucze[p][q] = 0;
+				q = 0;
+				break;
+			}
+			else
+			{
+				q++;
+			}
+		}
+		p++;
+	}
 
-    }
+	p = 0;
+	q = 0;
+	// SPRAWDZAM ZAWARTOŒÆ TABLICY KLUCZE
+	printf("\n \n wypisz: \n");
 
-    */
-/*
-//zawijanie dookola alfabetu:
-przesun = przesun % ('Z' - 'A');//zeby nie wyjechac dalej niz trzeba
+	while (p<10)
+	{
+		while (q<10)
+		{
+			if (klucze[p][q] != '#')
+			{
+				printf("%c", klucze[p][q]);
+			}
+			if (klucze[p][q] == '#')
+			{
+				p = 11;
+				break;
+			}
+			if (klucze[p][q] == 0)
+			{
+				q = 0;
+				break;
+			}
+			else
+			{
+				q++;
+			}
+		}
+		p++;
+	}
+	printf("\n---- \n");
+	// ZAMYKAM SZYFR I KOPIÊ I OTWIERAM W TRYBIE READ
+	printf("\n");
+	fclose(szyfr);
+	fclose(kopia);
+	kopia = fopen("kopia.txt", "r");
+	szyfr = fopen("szyfr.txt", "r");
 
-char znak;//duza litera
+	//odszyfruj(kontener, przesun);
+	/*
+	for(int i=0; i<=100; i++)
+	{   if()
+	fscanf(szyfr, "%c", &kontener);
 
-if (znak < 'A')
+	}
+
+	*/
+	/*
+	//zawijanie dookola alfabetu:
+	przesun = przesun % ('Z' - 'A');//zeby nie wyjechac dalej niz trzeba
+
+	char znak;//duza litera
+
+	if (znak < 'A')
 	znak += 'Z' - 'A';
-if (znak > 'Z')
+	if (znak > 'Z')
 	znak -= 'Z' - 'A';
-*/
-// SPRAWDZAM ZAWARTOŒÆ SZYFR.TXT
-       for(int i=0; i<=127; i++){
-       fscanf(szyfr, "%c", &kontener);
-        printf("%c", kontener);
-       }
-       printf("\n");
+	*/
+	// SPRAWDZAM ZAWARTOŒÆ SZYFR.TXT
+	for (int i = 0; i <= 127; i++) {
+		fscanf(szyfr, "%c", &kontener);
+		printf("%c", kontener);
+	}
+	printf("\n");
 
-       fclose(szyfr);
-       fclose(oryginal);
-       fclose(kopia);
-       fclose(klucz);
+	fclose(szyfr);
+	fclose(oryginal);
+	fclose(kopia);
+	fclose(klucz);
 
 
-return 0;
+	return 0;
 }
-char cezar(char kontener, int przesun){
+char cezar(char kontener, int przesun) {
 
-        if(kontener<='Z'&&kontener>='A'){
-        przesun = przesun % ('Z' - 'A');
+	if (kontener <= 'Z'&&kontener >= 'A') {
+		przesun = przesun % ('Z' - 'A');
 
-        if (kontener < 'A')
-        kontener += 'Z' - 'A';
-        if (kontener > 'Z')
-	    kontener -= 'Z' - 'A';
-
-
-return kontener;
-
-}
+		if (kontener < 'A')
+			kontener += 'Z' - 'A';
+		if (kontener > 'Z')
+			kontener -= 'Z' - 'A';
 
 
+		return kontener;
+
+	}
 
 
 
 
-return kontener;
+
+
+	return kontener;
 
 
 
